efb4f1180a7a1b5f2d247af89daed04830503f62
ls.mottet@gmail.com <ls.mottet@gmail.com>
2023-08-02 10:26:21 +0000

    margin calculation

--- models/sales.sql
+++ models/sales.sql
@@ -13,13 +13,14 @@ SELECT
   ,s.pdt_id AS products_id
   ###########
 	-- qty --
-	,s.quantity AS qty
+  ,s.quantity AS qty
   -- revenue --
   ,s.revenue AS turnover
   -- cost --
   ,CAST(p.purchSE_PRICE AS FLOAT64) AS purchase_price
-	,ROUND(s.quantity*CAST(p.purchSE_PRICE AS FLOAT64),2) AS purchase_cost
+  ,ROUND(s.quantity*CAST(p.purchSE_PRICE AS FLOAT64),2) AS purchase_cost
 	-- margin --
-	,ROUND(s.revenue-s.quantity*CAST(p.purchSE_PRICE AS FLOAT64),2) AS product_margin
+  ,{{ margin_percent('s.revenue', 's.quantity*CAST(p.purchSE_PRICE AS FLOAT64)') }} AS product_margin_percent
+  ,{{ product_margin('s.revenue', 's.quantity*CAST(p.purchSE_PRICE AS FLOAT64)')}} AS product_margin
 FROM sales s
 INNER JOIN product p ON s.pdt_id = p.products_id
